9021b90d8ad97efd6e911cefb4c8d5941613c47b
DILSHAN <dilshanfonseka76@gmail.com>
2024-06-15 10:52:07 +0530

    Item form done

--- controller/home.js
+++ controller/home.js
@@ -32,3 +32,31 @@ $("#customer_nav").on('click',()=>{
     $('#orders_nav').removeClass('active-page');
     $('#order_details_nav').removeClass('active-page');
 });
+
+$("#item_nav").on('click',()=>{
+    $('#home').css({display:'none'});
+    $('#customer_section').css({display:'none'});
+    $('#item_section').css({display:'block'});
+    $('#order_section').css({display:'none'});
+    $('#order_details_section').css({display:'none'});
+
+    $('#home_nav').removeClass('active-page');
+    $('#customer_nav').removeClass('active-page');
+    $('#item_nav').addClass('active-page');
+    $('#orders_nav').removeClass('active-page');
+    $('#order_details_nav').removeClass('active-page');
+});
+
+$("#orders_nav").on('click',()=>{
+    $('#home').css({display:'none'});
+    $('#customer_section').css({display:'none'});
+    $('#item_section').css({display:'none'});
+    $('#order_section').css({display:'block'});
+    $('#order_details_section').css({display:'none'});
+
+    $('#home_nav').removeClass('active-page');
+    $('#customer_nav').removeClass('active-page');
+    $('#item_nav').removeClass('active-page');
+    $('#orders_nav').addClass('active-page');
+    $('#order_details_nav').removeClass('active-page');
+});
--- db/db.js
+++ db/db.js
@@ -2,3 +2,4 @@ export let customer_db =[];
 export let item_db =[];
 
 export let order_db =[];
+export let order_detail_db =[];
